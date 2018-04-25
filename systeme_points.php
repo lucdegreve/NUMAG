9ca48be4691d993a89167db6247285d97e3f77bd
Adrien Pajot <adrien.pajot@agro-bordeaux.fr>
2018-04-25 12:02:19 +0200

    Chgt syst pts

--- systeme_points.php
+++ systeme_points.php
@@ -3,7 +3,17 @@
 	<meta charset="UTF-8">
 	<!-- page de fonctions pour le calcul des points -->
 	<!-- récupération points à partir des choix du profil -->
+	
 	<!-- fonction actualités -->
+	
+	<!-- Il y a une étoile pour choisir si une actualité nous intéresse ou pas 
+		Si ce bouton est sélectionné get les tags associés à l'identifiant de l'actu 
+		Faire une liste de ces tags
+		Comparer la liste de ces tags à celle de l'utilisateur. 
+		Si il y a une égalité (booléen) ajouter un point au score du tag de l'agriculteur 
+		Si non ajouter la valeur (le libbelé) à la liste des libellés de l'agriculteur en lui ajoutant un point à son score 
+		
+		Puis classement de cette nouvelle liste ? (utiliser la fonction classement si besoin mais pas forcément besoin parce qu'il faut classer avant d'afficher mais pas classer dans la base de donnée) 
 	<!-- fonction connection entre 2 profils -->
 	<!-- classement des mots clés ordre décroissant de points -->
 	
