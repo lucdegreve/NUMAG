5efd9c40ad37afe6cc9f27b189196e41024d663c
MC <mariecharlotte.oudot@agro-bordeaux.fr>
2018-04-25 17:06:36 +0200

    accueil connecté

---
+++ Accueil.php
@@ -0,0 +1,34 @@
+<html>
+
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>
+		Accueil
+	</title>
+	<!-- Déclaration de la feuille de style -->
+	<link rel="stylesheet" type="text/css" href="RACINE/css_accueil.css" media="all" />
+</head>
+
+<body>
+	<!-- DIV Entête-->
+	
+	
+<!-- On définit ici une section 'global' -->
+<div id="suggestions">
+Suggestions
+</div>
+
+<div id="contacts">
+Contacts
+</div>
+
+<div id="actualites">
+Actualites
+</div>
+
+
+
+	<!-- DIV Pied de page -->
+	
+</body>
+</html>
