a24497119e2faa54db13e33b966bc84989889267
Elsa <elsa.guilley@agro-bordeaux.fr>
2018-04-27 14:10:01 +0200

    Modif accueil connecté

--- accueil_connecté.php
+++ accueil_connecté.php
@@ -27,15 +27,17 @@ ZAZA & MC
 	$link=mysqli_connect();
 	//Afficher correctement les caractères spéciaux 
 	mysqli_set_charset($link, 'UTF-8');
-	//Construction des requêtes
-	$requete_conv_pr="SELECT distinct nom_ind FROM Individus, Messages_prives
-	WHERE (Messages_prives.id_dest=Individus.id_ind OR Messages_prives.id_expe=Individus.id_ind)
+	//Construction des requêtes RECUPERER L'IDENTIFIANT DU COMPTE (PAGE PRECEDENTE = CONNEXION) 
+	$requete1_conv_privee="SELECT distinct id_dest, id_expe FROM Individus, Messages_prives
+	WHERE Individus.id_ind=... 
+	AND (Messages_prives.id_dest=Individus.id_ind OR Messages_prives.id_expe=Individus.id_ind)"
+	$requete2_conv_privee="SELECT nom_ind, prenom FROM Individus
+	WHERE id_ind =..."
 	
-	"
 	$requete_conv_grp="SELECT titre_mp FROM Messages_prives, Individus
 	WHERE 
 	AND length(titre_mp)>0 
-	AND (Messages_prives.id_dest=Individus.id_ind OR Messages_prives.id_expe=Individus.id_ind)" //HELP
+	AND (Messages_prives.id_dest=Individus.id_ind OR Messages_prives.id_expe=Individus.id_ind)"
 	
 	?>
 	</div>
