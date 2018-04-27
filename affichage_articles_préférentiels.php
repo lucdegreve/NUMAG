91c0276aae3559b12f42519522b1b0a76ca7c1b2
Adrien Pajot <adrien.pajot@agro-bordeaux.fr>
2018-04-27 10:25:27 +0200

    Fin du script pour tri actu
    Maintenant reste plus que l'affichage au bon endroit en fonction de l'utilisateur

--- affichage_articles_préférentiels.php
+++ affichage_articles_préférentiels.php
@@ -1,40 +1,23 @@
 <html>
 <head>
 	<meta charset="UTF-8">
-	<!-- proposition d'articles en fonction du nombre de points par mots clés -->
-	
-	
+	<!-- Proposition d'actualites en fonction du nombre de points obtenus à partir des préférences des utilisateurs -->
 	
 </head>
 <body>
 	<?php
-	Classer les tags de lutilisateur 
-	$query_SU="SELECT * FROM Centres_interet
-	WHERE id_ind= XXXXX (id_individu de toute la session donc ça à finaliser à la fin)
-	ORDER BY Compteur"; // SU=Score utilisateur   
-	$result_SU=mysqli_query($link,$query_SU);
-	$tab_SU=mysqli_fetch_all($result_SU);
-	$nblig_SU=mysqli_num_rows($result_SU);
-	$nbcol_SU=mysqli_num_fields($result_SU);
-	
-	Récupérer la liste des actualités avec les tags associés 
-	$query_act="SELECT id_mot_cle FROM Actualites, mot_cle_actualite
-	WHERE id_mot_cle
-	id_actu 
-	id_ind= XXXXX (id_individu de toute la session donc ça à finaliser à la fin)
-	ORDER BY Compteur"; // act=actualités   
-	$result_act=mysqli_query($link,$query_act);
-	$tab_act=mysqli_fetch_all($result_act);
-	$nblig_act=mysqli_num_rows($result_act);
-	$nbcol_act=mysqli_num_fields($result_act);
-	
-	Noter les actualités 
-	
+	// Récuperation des scores des actualités ordonnés 
+	$query_SA="SELECT id_actu, SUM(Centres_interet.Compteur) as Score_actu
+	WHERE Centres_interet.id_mot_cle=Mots_cles.id_mot_cle
+	AND Mots_cle.id_mot_cle=Mot_cle_actualite.id_mot_cle
+	GROUP BY id_actu
+	ORDER BY Score_actu"; // SA=Score actualité 
+	$result_SA=mysqli_query($link,$query_SA);
+	$tab_SA=mysqli_fetch_all($result_SA);
+	$nblig_SA=mysqli_num_rows($result_SA);
+	$nbcol_SA=mysqli_num_fields($result_SA);
 
-	Classer les actualités 
-	
-	
-	Les afficher dans l'ordre 
+	//$tab_SA : il suffit d'afficher $tab_SA puisque celui-ci est déjà trié dans l'ordre des points des actu 
 	?>
 </body>
-</html>
+</html> 
