f088ebbdfb46081891c6566f7cf30627d34e89aa
Adrien Pajot <adrien.pajot@agro-bordeaux.fr>
2018-04-30 09:31:41 +0200

    Affichages projet et actu (classement)

--- Affichage_projets_preferentiels.php
+++ Affichage_projets_preferentiels.php
@@ -8,17 +8,17 @@
 	<?php
 	// Récuperation des scores des actualités ordonnés 
 	// ATTENTION A BIEN RENTRER LE BON ID_IND ET NON SEULEMENT L'ID 1
-	$query_SA="SELECT id_actu, SUM(Centres_interet.Compteur) AS Score_actu 
-	FROM Centres_interet, Mots_cles, mot_cle_actu 
+	$query_SP="SELECT id_proj, SUM(Centres_interet.Compteur) AS Score_projet
+	FROM Centres_interet, Mots_cles, mot_cle_projet 
 	WHERE Centres_interet.id_ind=1 
 	AND Centres_interet.id_mot_cle=Mots_cles.id_mot_cle 
-	AND Mots_cles.id_mot_cle=Mot_cle_actu.id_mot_cle 
-	GROUP BY id_actu 
-	ORDER BY Score_actu DESC"; // SA=Score actualité 
-	$result_SA=mysqli_query($link,$query_SA);
-	$tab_SA=mysqli_fetch_all($result_SA);
-	$nblig_SA=mysqli_num_rows($result_SA);
-	$nbcol_SA=mysqli_num_fields($result_SA);
+	AND Mots_cles.id_mot_cle=Mot_cle_projet.id_mot_cle 
+	GROUP BY id_proj
+	ORDER BY Score_projet DESC"; // SA=Score actualité 
+	$result_SP=mysqli_query($link,$query_SP);
+	$tab_SP=mysqli_fetch_all($result_SP);
+	$nblig_SP=mysqli_num_rows($result_SP);
+	$nbcol_SP=mysqli_num_fields($result_SP);
 
 	//$tab_SA : il suffit d'afficher $tab_SA puisque celui-ci est déjà trié dans l'ordre des points des actu 
 	?>
