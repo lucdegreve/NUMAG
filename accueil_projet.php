0fc3c7ccb06f78420d1068a7b9a5722a1f2984a5
Marie Desmaison <marie.desmaison@agro-bordeaux.fr>
2018-04-30 11:05:37 +0200

    stage Mise à jour

--- accueil_projet.php
+++ accueil_projet.php
@@ -15,34 +15,45 @@ Projet
 
 <td>
 <?php
+	$link=mysqli_connect('localhost','root','','bdd_racine');
+
 	// Recherche de projets à base de liste déroulante
-	$query="SELECT  libelle_mots_cles, nom_dpt, libelle_statut, duree, libelle_sout
-	FROM Projets 
-	INNER JOIN Individus Projets.id_ind=Individus.id_ind
-	INNER JOIN Communes Individus.id_commune=Communes.id_commune
-	INNER JOIN Departements Communes.id_dpt=Departements.id_dpt
-	INNER JOIN Types_soutien Projets.id_sout=Types_soutien.id_sout";
+	$query="SELECT  libelle_mot_cle, nom_dpt, libelle_statut,
+	duree, libelle_sout FROM projets
+	INNER JOIN mot_cle_projet 
+	ON projets.id_proj=mot_cle_projet.id_proj
+	INNER JOIN mots_cles 
+	ON mot_cle_projet.id_mot_cle=mots_cles.id_mot_cle
+	INNER JOIN individus 
+	ON projets.id_ind=individus.id_ind
+	INNER JOIN communes 
+	ON individus.id_commune=communes.id_commune
+	INNER JOIN departements 
+	ON communes.id_dpt=departements.id_dpt
+	INNER JOIN types_soutien 
+	ON projets.id_sout=types_soutien.id_sout
+	INNER JOIN statuts 
+	ON projets.id_statut=statuts.id_statut";
 	
 	$result=mysqli_query($link,$query);
-	
 	$Tab=mysqli_fetch_all($result);
 	$nbligne=mysqli_num_rows($result);
 	$nbcol=mysqli_num_fields($result);
 	
-	echo "<FORM action="Resultat_recherche_projet.php" method="GET"  name="form2">";
+	echo "<FORM action='Resultat_recherche_projet.php' method='GET'  name='form2'>";
 			
-	for ($i=0, $i<nbligne, $i++)
+	for ($i=0; $i<$nbligne; $i++)
 	{
-		for ($j=0,$j<nbcol, $j++)
+		for ($j=0; $j<$nbcol; $j++)
 		{
 			echo $Tab[$i][$j];
 			echo "<select name='listetag'>";
-			echo ("<option value= ".$Tab[$i][$j].">".$Tab[$i][$j]."</option>");
-			echo <br/>;
+			echo "<option value= '.$Tab[$i][$j].'>'.$Tab[$i][$j].'</option>";
+			echo "<br/>";
 		}
 	}
 	
-	echo <INPUT TYPE=SUBMIT  value='Valider'> ;
+	echo "<INPUT TYPE=SUBMIT  value='Valider'>" ;
 
 
 ?>
@@ -62,11 +73,11 @@ $query="SELECT  titre_proj, libelle_statut
 	$nbligne=mysqli_num_rows($result);
 	$nbcol=mysqli_num_fields($result);
 	
-	echo <table>;
+	echo "<table>";
 	
-	for ($i=0, $i<$nbligne, $i++)
+	for ($i=0; $i<$nbligne; $i++)
 	{
-		echo '<tr>
+		echo "<tr>
 		'.$Tab[$i][0].'
 		</tr>;
 		<tr>;
@@ -76,10 +87,10 @@ $query="SELECT  titre_proj, libelle_statut
 		<td>
 		<a href='projet.php?projet=$id_proj'> Consulter </a>
 		</td>
-		</tr>';
+		</tr>";
 	}
 	
-	echo </table>;
+	echo "</table>";
 	?>
 </td>
 </table>
