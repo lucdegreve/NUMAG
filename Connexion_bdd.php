b89d9b30145be1893f2167358da59a2665572916
Adrien Pajot <adrien.pajot@agro-bordeaux.fr>
2018-05-02 13:53:53 +0200

    Auto stash before merge of "master" and "origin/master"

--- Connexion_bdd.php
+++ Connexion_bdd.php
@@ -1,6 +1,6 @@
 <?
 	//Page de connexion à la base de donnée 
-	$link=mysqli_connect('194.199.251.65', 'root', 'numag2018', 'bdd_racine_beta_27.04')
+	$link=mysqli_connect('localhost', 'root', 'numag2018', 'bdd_racine_beta_27.04')
 	if (mysqli_connect_errno()) {
 	printf("Echec de la connexion: %s\n", mysqli_connect_error());
 	exit();
