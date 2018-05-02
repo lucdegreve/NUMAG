31314348c549f2c07587b9d7117d8d001d582298
Luc <lucdegreve@hotmail.fr>
2018-05-02 09:07:14 +0200

    Page Alerte fin

--- Racine/gabarits/Alerte.php
+++ Racine/gabarits/Alerte.php
@@ -22,79 +22,96 @@
 	<!-- Navigation (Menus) -->
 	<?php include("DIVNavigation.html"); ?>
 
+	
 <?php
 header('Content-Type: text/html; charset=UTF-8');
-$link=mysqli_connect('localhost','root','','oiseaudb2018');
+$link=mysqli_connect('localhost','root','','bdd_racine_beta_27.04');
 //mise en place de la requete
-$idobs=$_GET["listenomobs"];
-$nomdpt=$_GET["listedpt"];
-$queryProjet="SELECT Alertes_Projet.id_ind, Alertes_Projet.id_proj, Alertes_Projet.Date_Alrt, Individus.nom_ind, Individus.prenom, Individus.id_ind 
-FROM Alertes_Projet, Individus 
-where Alertes_Projet.id_ind=Individus.id_ind" ;
 
-$queryStage="SELECT Alertes_Stage.id_ind, Alertes_Stage.id_proj, Alertes_Stage.Date_Alrt, Individus.nom_ind, Individus.prenom, Individus.id_ind 
-FROM Alertes_Stage, Individus 
-where Alertes_Stage.id_ind=Individus.id_ind" ;
+//requete projet
+$queryProjet="SELECT Alertes_Projet.id_ind, Alertes_Projet.id_proj, Alertes_Projet.Date_Alp, Individus.nom_ind, Individus.prenom, Individus.id_ind, projets.id_proj, projets.titre_proj, Alertes_Projet.vu_proj, projets.url_proj 
+FROM Alertes_Projet, Individus, projets
+where Alertes_Projet.id_ind=Individus.id_ind and projets.id_proj=Alertes_Projet.id_proj and Alertes_Projet.vu_proj=0";
+
+
+
+//requete stage
+$queryStage="SELECT Alertes_Stage.id_ind, Alertes_Stage.id_st, Alertes_Stage.Date_Als, Individus.nom_ind, Individus.prenom, Individus.id_ind, stages.id_st, stages.titre_st, Alertes_Stage.vu_st, stages.url_st
+FROM Alertes_Stage, Individus, stages 
+where Alertes_Stage.id_ind=Individus.id_ind and stages.id_st=Alertes_Stage.id_st and Alertes_Stage.vu_st=0" ;
 
 
 $resultP=mysqli_query($link, $queryProjet);
 $resultS=mysqli_query($link, $queryStage);
 
-echo '<table border=1>';
-//mise en place des en-tetes
-for($i=0; $i<2; $i++)
-{
-	$champ=mysqli_fetch_field_direct($resultP,$i)->name;
-			echo"<td>";
-			echo $champ;
-			echo"</td>";
-}
+echo '<table border=0>';
+echo'</br>';
+echo'</br>';
+echo 'Projet';
+echo'</br>';
+echo'</br>';
+
 
 //tableau d'alertes projets
 while($row=mysqli_fetch_array($resultP,MYSQLI_BOTH))
 {
+	echo '<form action=Alertevup.php METHOD=get>';
 	$nom =$row["nom_ind"];
 	$prenom =$row["prenom"];
 	$titre =$row["titre_proj"];
+	$idp =$row["id_proj"];
+	$url=$row["url_proj"];
 	echo'<tr>';
 	echo'<td>';
 	echo $prenom,' ', $nom ,' a commenté votre projet nommé ', $titre;
 	echo'</td>';
 	echo'<td>';
-	
-//	echo'</td>';
-//	echo'</tr>';
+	echo '	<input TYPE="hidden" name="url" value='.$url.' >';
+	echo '	<input TYPE="hidden" name="idp" value='.$idp.' >';
+	echo '	<input TYPE="SUBMIT" name="voir" value="voir" >';
+	echo'</td>';
+	echo'</tr>';
+	echo '</form>';
 }	
+
 echo'</tr>';
 echo'</table>';
+echo'</br>';
+echo'</br>';
 
-echo '<table border=1>';
-//mise en place des en-tetes
-for($i=0; $i<2; $i++)
-{
-	$champ=mysqli_fetch_field_direct($resultP,$i)->name;
-			echo"<td>";
-			echo $champ;
-			echo"</td>";
-}
+echo '<table border=0>';
+echo'Stage';
+echo'</br>';
+echo'</br>';
+
 
 //tableau d'alertes stages
 while($row=mysqli_fetch_array($resultS,MYSQLI_BOTH))
 {
+	echo '<form action=Alertevus.php METHOD=get>';
 	$nom =$row["nom_ind"];
 	$prenom =$row["prenom"];
-	$titre =$row["titre_proj"];
+	$titre =$row["titre_st"];
+	$ids =$row["id_st"];
+	$url=$row["url_st"];
 	echo'<tr>';
 	echo'<td>';
 	echo $prenom,' ', $nom ,' a commenté votre stage nommé ', $titre;
 	echo'</td>';
-	echo'<td>';
-	
-//	echo'</td>';
-//	echo'</tr>';
+	echo '<td>';
+	echo '	<input TYPE="hidden" name="url" value='.$url.' >';
+	echo '	<input TYPE="hidden" name="ids" value='.$ids.' >';
+	echo '	<input TYPE="SUBMIT" name="voir" value="voir" >';
+	echo'</td>';
+	echo'</tr>';
+	echo '</form>';
 }	
+
+
 echo'</tr>';
 echo'</table>';
+echo'</br>';
+echo'</br>';
 ?>
 	<!-- Pied de page -->		
 	<?php include("Pied_de_page.html"); ?>	
