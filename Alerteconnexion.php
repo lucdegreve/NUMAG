01ffb45e03c86f6f6cf703a0b9c579057be3c58a
Adrien Pajot <adrien.pajot@agro-bordeaux.fr>
2018-05-03 09:13:06 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ Alerteconnexion.php
@@ -0,0 +1,35 @@
+<?php
+	session_start();
+	$mail=$_GET["mail"];
+	$mdp=$_GET["mdp"];
+	$url1="http://194.199.251.65/NUMAG/Racine/gabarits/Index.php";
+	$url2="http://194.199.251.65/NUMAG/Racine/gabarits/connexion-inscription.php";
+	$link=mysqli_connect('localhost','root','','racine');
+
+	$query_ind="SELECT id_ind, mail, mdp
+		FROM Individus"; 
+	$result_ind=mysqli_query($link,$query_ind);
+	$tab_ind=mysqli_fetch_all($result_ind);
+	$nblig_ind=mysqli_num_rows($result_ind);
+	$nbcol_ind=mysqli_num_fields($result_ind);
+	$VERIF=0;
+	for($k=0; $k<$nblig_ind; $k++)
+	{
+		if ($tab_ind[$k][1]==$mail AND $tab_ind[$k][2]==$mdp)
+		{
+			$VERIF=1;
+			$ligne=$k;
+		}
+	}
+	if ($VERIF==1)
+	{
+		header('Location: '.$url1);
+		exit();
+		$_SESSION['id_ind_co']=$tab_ind[$ligne][0];
+	}
+	else
+	{
+		header('Location: '.$url2);
+		exit();
+	}
+?>
