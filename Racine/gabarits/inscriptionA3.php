47bd78552467b3ce604612d66f57a37312c972aa
Luc <lucdegreve@hotmail.fr>
2018-05-03 10:46:05 +0200

    Maj A3

--- Racine/gabarits/inscriptionA3.php
+++ Racine/gabarits/inscriptionA3.php
@@ -5,119 +5,56 @@ Cette page est la dernière des trois pages de formulaire d'inscription à rempl
 Il suffit de cocher les cases voulues
 Des validations sont mises en place grace aux données de bootstrap-->
 
-<!DOCTYPE html> 
+<!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <title> Formulaire d'inscription </title>
-  
-    <!-- css -->
-
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/style.css">
-  </head>
-  <body>
-    <?php include("Entete-VALIDE.html"); ?>
-    <br/>
-    <br/>
-    <div class="container">
-      <div class="card border-info mb-3">
-        <span style="color: info;">
-        <div class="card-header">Formulaire d'inscription pour agriculteurs</div>
-        </span>
-          <div class="container">
-            <br/>
-            Veuillez renseigner vos centres d'interets
-            <br/>
-            <br/>
-          </div>
-          <div class="container">
-            <div class="form-row">
-              <form action="Tentative.php" method="post">
-                <div class="form-group col-md-4">
-                  <div class="form-check">
-                      <input class="form-check-input" type="checkbox" value="Pintade" name="centre[]">
-                      <label class="form-check-label" for="gridCheck">
-                      Pintade
-                      </label>
-                  </div>
-                  <div class="form-check">
-                      <input class="form-check-input" type="checkbox" value="Orge" name="centre[]">
-                      <label class="form-check-label" for="gridCheck">
-                      Orge
-                      </label>
-                  </div>
-                  <div class="form-check">
-                      <input class="form-check-input" type="checkbox" value="Apiculture" name="centre[]">
-                      <label class="form-check-label" for="gridCheck">
-                      Apiculture
-                      </label>
-                  </div>
-                  <div class="form-check">
-                      <input class="form-check-input" type="checkbox" value="Maraichage" name="centre[]">
-                      <label class="form-check-label" for="gridCheck">
-                      Maraichage
-                      </label>
-                  </div>
-                </div>
-                <div class="form-group col-md-4">
-                  <div class="form-check">
-                      <input class="form-check-input" type="checkbox" value="Ecologie" name="centre[]">
-                      <label class="form-check-label" for="gridCheck">
-                      Ecologie
-                      </label>
-                  </div>
-                  <div class="form-check">
-                      <input class="form-check-input" type="checkbox" value="Oléoprotéagineux" name="centre[]">
-                      <label class="form-check-label" for="gridCheck">
-                      Oléoprotéagineux
-                     </label>
-                  </div>
-                  <div class="form-check">
-                      <input class="form-check-input" type="checkbox" value="Viticulture" name="centre[]">
-                      <label class="form-check-label" for="gridCheck">
-                      Viticulture
-                      </label>
-                  </div>
-                  <div class="form-check">
-                      <input class="form-check-input" type="checkbox" value="INRA" name="centre[]">
-                      <label class="form-check-label" for="gridCheck">
-                      INRA
-                      </label>
-                  </div>
-                </div>
-                <div class="form-group col-md-4">
-                  <div class="form-check">
-                      <input class="form-check-input" type="checkbox" value="Legumes" name="centre[]">
-                      <label class="form-check-label" for="gridCheck">
-                      Legumes
-                      </label>
-                  </div>
-                  <div class="form-check">
-                      <input class="form-check-input" type="checkbox" value="Fraises" name="centre[]">
-                      <label class="form-check-label" for="gridCheck">
-                      Fraises
-                      </label>
-                  </div>
-                  <div class="form-check">
-                      <input class="form-check-input" type="checkbox" value="Machinisme" name="centre[]">
-                      <label class="form-check-label" for="gridCheck">
-                      Machinisme
-                      </label>
-                  </div>
-                  <div class="form-check">
-                      <input class="form-check-input" type="checkbox" value="Limousine" name="centre[]">
-                      <label class="form-check-label" for="gridCheck">
-                      Aubrac
-                      </label>
-                  </div>
-                </div>
-				<a href="connexion.php" class="btn btn-info"> Enregistrer </a>
-                <!--  <input class="btn btn-info" type="submit" value="Enregistrer"> -->
-              </form>
-            </div>
-          </div>
-      </div>
-    </div>
-    <?php include("Pied-VALIDE.html"); ?>
-  </body>
+	<head>
+		<meta charset="utf-8">
+		<title> Formulaire d'inscription </title>
+		
+		<!-- css -->
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<link rel="stylesheet" href="css/style.css">
+		
+		<?PHP 
+		$link = mysqli_connect('localhost','root','','bdd');
+		$query = "SELECT id_mot_cle,libelle_mot_cle FROM mots_cles";
+		$result=mysqli_query($link,$query);
+		?>
+	</head>
+	<body>
+		<?php include("Entete-VALIDE.html"); ?>
+		<br/>
+		<br/>
+		<div class="container">
+			<div class="card border-info mb-3">
+				<span style="color: info;">
+				<div class="card-header">Formulaire d'inscription pour agriculteurs</div>
+				</span>
+					<div class="container">
+						<br/>
+						veuillez renseigner vos centres d'interets
+						<br/>
+						<br/>
+					</div>
+					<div class="container">
+						<div class="form-row">
+							<form action="Tentative.php" method="post">
+									<?php
+										while($row=mysqli_fetch_array($result,MYSQLI_BOTH))
+											{
+												echo'<div class="form-check">';
+												$id=$row["id_mot_cle"];
+												$nom =$row["libelle_mot_cle"];
+												echo'<input class="form-check-input" type="checkbox" name="centre[]" value='.$nom.' >';
+												echo "<option> ".$nom." </option>";
+												echo'</div>';
+											}	
+										?>
+								<input type="submit" class="btn btn-info" value="suite" ></input>
+							</form>
+						</div>
+					</div>
+			</div>
+		</div>
+		<?php include("Pied-VALIDE.html"); ?>
+	</body>
