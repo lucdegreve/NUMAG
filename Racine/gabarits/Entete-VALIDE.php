a26dae6260aacf53992667979fa9503597460df1
NumagServeur <luc.degreve@agro-bordeaux.fr>
2018-05-04 11:20:34 +0200

    maj entete bdd

--- Racine/gabarits/Entete-VALIDE.php
+++ Racine/gabarits/Entete-VALIDE.php
@@ -52,7 +52,7 @@ Bootstrap assure un aspect graphique élégant-->
 
 				<?php
 					header('Content-Type: text/html; charset=UTF-8');
-					$link=mysqli_connect('localhost','root','numag2018','bdd_racine_beta_27.04.5');
+					include'Connexion_bdd.php';
 					//mise en place de la requete
 
 					//requete projet
