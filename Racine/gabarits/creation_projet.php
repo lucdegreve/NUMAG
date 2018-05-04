77e42e44107c201c8d480af4111bc33770e8053a
Duval <mayeul.duval@agro-bordeaux.fr>
2018-05-04 17:23:21 +0200

    Création Projet + tags

--- Racine/gabarits/creation_projet.php
+++ Racine/gabarits/creation_projet.php
@@ -1,101 +1,96 @@
 	<!--Script par Manuel, Julien et Marie-->
 	<head>
-	<?php Include("Entete-VALIDE.php");?>
-	<br/>
-	<br/>
+		<?php Include("Entete-VALIDE.php");?>
+		<br/>
+		<br/>
+		<title>Création de projet</title>
+		<script type="text/javascript">
+	        function open_popup_test()
+	        {{
+	        window.open("recherche_tag_st1.php", "recherche_tag_st", "toolbar=yes", "status=yes", "scrollbars=yes", "resizable=yes", "fullscreen=yes");
+			}
+	       // window.close();
+			}
 
-	<script type="text/javascript">
-        function open_popup_test()
-        {{
-        window.open("recherche_tag_st1.php", "recherche_tag_st", "toolbar=yes, status=yes, scrollbars=yes, resizable=no, width=300, height=300");
-		}   
-       // window.close();
-		}
-        
-		function valider(){
-				var ok=1;
-				message ="";
-				if (document.form2.titre_proj.value =="") 
-				{
-					ok=ok-1;
-					message =message + "Veuillez saisir un titre de projet \n";
+			function valider(){
+					var ok=1;
+					message ="";
+					if (document.form2.titre_proj.value =="")
+					{
+						ok=ok-1;
+						message =message + "Veuillez saisir un titre de projet \n";
+					}
+					if (document.form2.desc_proj.value =="")
+					{
+						ok=ok-1;
+						message =message + "Veuillez saisir une description \n";
+					}
+					if (document.form2.date_proj.value =="")
+					{
+						ok=ok-1;
+						message =message + "Veuillez saisir une date de début de projet \n";
+					}
+					if (document.form2.duree.value =="")
+					{
+						ok=ok-1;
+						message =message + "Veuillez saisir une durée du projet \n";
+					}
+					if (ok==1)
+					{
+						return true;
+					}
+					else
+					{
+						alert(message);
+						return false ;
+					}
 				}
-				if (document.form2.desc_proj.value =="") 
-				{
-					ok=ok-1;
-					message =message + "Veuillez saisir une description \n";
-				}
-				if (document.form2.date_proj.value =="") 
-				{
-					ok=ok-1;
-					message =message + "Veuillez saisir une date de début de projet \n";
-				}
-				if (document.form2.duree.value =="") 
-				{
-					ok=ok-1;
-					message =message + "Veuillez saisir une durée du projet \n";
-				}
-				if (ok==1)
-				{
-					return true;
-				}
-				else 
-				{
-					alert(message);
-					return false ;
-				}
-			}	
-		</script>
-        </head>
+			</script>
+	        </head>
 	<body>
-	<form method="GET" action="previsualisation.php" onsubmit="return valider()" name='form2'> 
-	<table border=1 style="width:1%">
-	<tr><td>
-	Titre du projet						<!-- Création du premier tableau à remplir par l'exploitant -->
-	</td></tr><tr><td>
-	<INPUT TYPE='TEXT' name='titre_proj'>
-	</td></tr><tr><td>
-	Description du projet
-	</td></tr><tr><td>
-	<INPUT TYPE='TEXT' name='desc_proj'>
-	</td></tr><tr>	<td>
-	Durée du projet
-	</td></tr><tr><td>
-	<INPUT TYPE='TEXT' name='duree' >
-	</td></tr><tr><td>
-	Date de publication
-	</td></tr>
-	<tr><td>
-	<INPUT TYPE='date' name='date_proj'>
-	</td></tr>
-	</table>
-	 
-	<table border=1 style="width:1%">					<!-- Création du deuxième tableau à rentrer par l'agriculteur. Il contient aussi une liste déroulante -->
-	<tr><td>
-	Statut du projet
-	</td></tr>
-	<tr><td>
-	<?php
-			echo '<select name=etatprojet>';
-			$etatprojet=array('Non-débuté','En-cours','Terminé');
-			for ($i=0; $i<=2; $i++)
-				echo ("<option value= ".$etatprojet[$i].">".$etatprojet[$i]."</option>");
-	?>
-	</td></tr><tr><td>
-	Lien Mimosa
-	</td></tr>
-	<tr><td>
-	<INPUT TYPE='TEXT' name='lien_mimosa'>
-	</td></tr>
-	</table>
-	<TD><a href="javascript:open_popup_test()">Ajouter des mots clés</a><br><br></TD>
-	 
-	<INPUT TYPE="SUBMIT" name="bt_submit" value="Visualiser"><!-- Création du bouton valider-->
-	</form>
-	</body>
-	<br/>
-	<br/>
-	<br/>
-	<?php Include("Pied-VALIDE.html");?>
-	
+		<div class="container">
+			<div class="jumbotron">
+				<h2>Création de projet</h2>
+				<hr class="my-4">
+				<form method="GET" action="previsualisation.php" onsubmit="return valider()" name='form2'>
+					<div class="row">
+						<div class="col-lg-6">
+							<div class="form-group">
+								<input type="text" class="form-control" name="titre_proj" placeholder="Entrer le titre">
+							</div>
+							<div class="form-group">
+								<input type="text" class="form-control" name="duree" placeholder="Entrer la durée">
+							</div>
+							<div class="form-group">
+								<label for="date_proj"><p class="minilead">Entrer la date de publication</p></label>
+								<input type="date" class="form-control" name="date_proj">
+							</div>
+							<div class="form-group">
+						    <label for="etatprojet"><p class="minilead">Entrer la date de publication</p></label>
+						    <select class="form-control" name="etatprojet">
+						      <option>Non-débuté</option>
+						      <option>En-cours</option>
+						      <option>Terminé</option>
+						    </select>
+						  </div>
+							<a href="javascript:open_popup_test()">Ajouter des mots clés</a>
+						</div>
+						<div class="col-lg-6">
+							<div class="form-group">
+									<input type="text" class="form-control HauteurAuto" name="desc_proj" placeholder="Entrer la description du projet">
+							</div>
+							<div class="form-group">
+								<input type="text" class="form-control VertMiimosa" name="lien_mimosa" placeholder="Lien Miimosa">
+							</div>
+						</div>
+					</div>
+					<hr class="my-4">
+					<input type="submit" class="btn btn-block btn-info btn-lg center-block"name="bt_submit" value="Créer">
+				</form>
+			</div>
+		</div>
+
+		<?php Include("Pied-VALIDE.html");?>
+</body>
+
 </html>
