ce69f9c2e02d5124eae50610c9a72df1d5b299a7
Adrien Pajot <adrien.pajot@agro-bordeaux.fr>
2018-05-04 13:58:05 +0200

    Auto stash before merge of "master" and "origin/master"

--- Racine/gabarits/test_checkbox.php
+++ Racine/gabarits/test_checkbox.php
@@ -2,13 +2,7 @@
 <?php
 session_start();
 
-if(isset($_GET['jury'])){ //sera vrai si au moins un moins un checkbox a été cochée
- 
-	/*foreach($_GET['jury'] as $choix){
- 
-		echo $choix."; "; //ex. : 12 16 23 31 ...							
-	}
-	*/
+if(isset($_GET['jury'])){ //sera vrai si au moins un checkbox a été coché
 	$tab=$_GET['jury'];
 	$_SESSION['tab']=$tab;
 	var_dump($tab);
