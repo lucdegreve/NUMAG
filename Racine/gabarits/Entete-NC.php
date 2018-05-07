fc1f898d781433a6c9446ddbb7f060afa4ce3ba8
Aurélie <aurelie.jambon@agro-bordeaux.fr>
2018-05-07 10:13:08 +0200

    modif entete

--- Racine/gabarits/Entete-NC.php
+++ Racine/gabarits/Entete-NC.php
@@ -32,7 +32,7 @@ Bootstrap assure un aspect graphique élégant-->
 		<div class="collapse navbar-collapse" id="navbarTogglerDemo01">
 			<ul class="navbar-nav mr-auto mt-2 mt-lg-0">
 				<li class="nav-item">
-					<a class="nav-link" href="Index.php"><p class="text-light">Accueil</p></a>
+					<a class="nav-link" href="Index.php"><p class="text-light"><img alt="" src="picto\accueil.png" width="40" height="40"></p></a>
 				</li>
 				<li class="nav-item">
 					<a class="nav-link" href="Index.php"><p class="text-light">Projets</p></a>
