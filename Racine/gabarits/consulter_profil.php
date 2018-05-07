86969310751bc8e80f706cdc705fa1db82532589
NumagServeur <luc.degreve@agro-bordeaux.fr>
2018-05-07 16:24:47 +0200

    projet fake profil

---
+++ Racine/gabarits/consulter_profil.php
@@ -0,0 +1,77 @@
+
+
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title> Benchmark </title>
+
+		<!-- css -->
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<link rel="stylesheet" href="css/style.css">
+	</head>
+	<body>
+		<?php include("Entete-VALIDE.html"); ?>
+		<br/>
+		<form>
+      <div class="container">
+				<div class="jumbotron">
+					<div class="row">
+
+						<div class="col-sm-4">
+							<div class="card border-danger">
+							  <img class="card-img-top" src="picto/apiculteur.jpg" alt="Card image cap">
+							  <div class="card-body">
+							    <h5 class="card-title">Alphonse Pommier</h5>
+							    <p class="card-text">Passionné d'apiculture depuis tout jeune. Je suis engagé dans la protection des abeilles. <br/> Comme disait Einstein : « Si l’abeille disparaissait de la surface du globe, l’homme n’aurait plus que quatre années à vivre »</p>
+							  </div>
+							  <ul class="list-group list-group-flush">
+							    <li class="list-group-item">01/01/1982</li>
+							    <li class="list-group-item">Balizac (33730)</li>
+							    <li class="list-group-item">Api Ferme</li>
+							  </ul>
+								<a href="benchmark.php" class="btn btn-danger">Membre Premium</a>
+							</div>
+						</div>
+
+						<div class="col-sm-8">
+							<div class="card">
+	      				<div class="card-body">
+	        				<h5 class="card-title"><b>Centres d'intérêts</b></h5>
+	        				<p class="card-text">Centres d'intérêts de Alphonse Pommier.</p>
+									<hr class="my-4">
+	        				<button href="#" class="btn btn-secondary btn-lg disabled" tabindex="-1" role="button" aria-disabled="true">Ecologie</button>
+									<button href="#" class="btn btn-secondary btn-lg disabled" tabindex="-1" role="button" aria-disabled="true">Apiculture</button>
+									<button href="#" class="btn btn-secondary btn-lg disabled" tabindex="-1" role="button" aria-disabled="true">Miel</button>
+									<button href="#" class="btn btn-secondary btn-lg disabled" tabindex="-1" role="button" aria-disabled="true">Abeilles</button>
+									<button href="#" class="btn btn-secondary btn-lg disabled" tabindex="-1" role="button" aria-disabled="true">Numérique</button>
+	      				</div>
+	    				</div>
+							<br/>
+							<div class="card">
+	      				<div class="card-body">
+	        				<h5 class="card-title"><b>Projets</b></h5>
+	        				<p class="card-text">Projets avec la participation de Alphonse Pommier.</p>
+									<hr class="my-4">
+	        				<a href="Projet_apiculteur.php"><i>Projet d'apiculture biologique</i></a><br/>
+									<a href="Projet_apiculteur.php"><i>Système de suivi des ruches pour lutter contre les vols</i></a>
+	      				</div>
+	    				</div>
+							<br/>
+							<div class="card">
+	      				<div class="card-body">
+	        				<h5 class="card-title"><b>Stages</b></h5>
+	        				<p class="card-text">Propositions de stages de Alphonse Pommier.</p>
+									<hr class="my-4">
+	        				<p class="card-text"> <i> Aucune offre de stage n'a été proposée par Alphonse Pommier. </i></p>
+	      				</div>
+	    				</div>
+
+  					</div>
+					</div>
+				</div>
+			</div>
+    </form>
+    <br/>
+    <br/>
+    <?php include("Pied-VALIDE.html"); ?>
+  </body>
