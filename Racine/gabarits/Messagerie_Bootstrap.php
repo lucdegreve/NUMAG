6d09374d0abbb62c2a3e2a6d1df4df1ed8bdb7ca
Nico <nicolas.tournant@agro-bordeaux.fr>
2018-05-07 09:39:32 +0200

    recherche nom et prenom 2

--- Racine/gabarits/Messagerie_Bootstrap.php
+++ Racine/gabarits/Messagerie_Bootstrap.php
@@ -73,7 +73,7 @@
                 JOIN (SELECT CASE WHEN id_dest=$id_ind_co THEN id_expe ELSE id_dest END AS id_compte, MAX(date_mp) AS inter, lu
                 FROM messages_prives WHERE (id_dest=$id_ind_co OR id_expe=$id_ind_co) GROUP BY id_compte) r
                 ON i.id_ind=r.id_compte
-                WHERE (nom_ind LIKE'%$RECHERCHE%' OR prenom LIKE'%$RECHERCHE%')
+                WHERE (nom_ind LIKE '%$RECHERCHE%' OR prenom LIKE'%$RECHERCHE%')
                 ORDER BY inter DESC";
 
                 $results1=mysqli_query($link,$query1);
