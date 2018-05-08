71c9c521740443847c30ef92bfcdb37b8d84f5f2
Duval <mayeul.duval@agro-bordeaux.fr>
2018-05-08 16:59:34 +0200

    Suite modif Api

--- Racine/gabarits/projet_api.php
+++ Racine/gabarits/projet_api.php
@@ -24,6 +24,7 @@ mysqli_set_charset($link, 'UTF-8');
         </div>
       </div>
     </div>
+		<br>
     <div class="jumbotron">
       <div class="row">
         <div class="col-lg-6">
@@ -54,61 +55,62 @@ mysqli_set_charset($link, 'UTF-8');
       <h5>Partie forum</h5>
 		</div>
 		<div class="row">
-      <br/><br/><br/>
-	  <?php
+			<div class="col-lg-12">
+				<?php
 
-			include ("connexion_bdd.php");
+					include ("connexion_bdd.php");
 
-			$forum = 0; // id du forum dans la table individus
-
-
-			if (@isset($_GET['bt']))
-			{
-				$bt=$_GET['bt'];
-			    $contact=$_GET['idcontact'];
-			    $message = $_GET['message'];
-			    $query2="INSERT INTO messages_prives (id_dest, id_expe, texte, date_mp, lu) VALUES ($contact, $id_ind_co, '$message', NOW(), 0)";
-			    $results2=mysqli_query($link,$query2);
-			}
+					$forum = 0; // id du forum dans la table individus
 
 
-			//Requête pour récupérer tous les messages envoyés au forum.
+					if (@isset($_GET['bt']))
+					{
+						$bt=$_GET['bt'];
+					    $contact=$_GET['idcontact'];
+					    $message = $_GET['message'];
+					    $query2="INSERT INTO messages_prives (id_dest, id_expe, texte, date_mp, lu) VALUES ($contact, $id_ind_co, '$message', NOW(), 0)";
+					    $results2=mysqli_query($link,$query2);
+					}
 
-			$query="SELECT individus.id_ind, individus.nom_ind, individus.prenom,
-			          messages_prives.id_mp, DATE_FORMAT(messages_prives.date_mp, '%d/%m/%y %H:%i') as date, messages_prives.lu, messages_prives.texte, id_dest
-			          FROM individus
-			          JOIN messages_prives
-			          ON individus.id_ind = messages_prives.id_expe OR individus.id_ind = messages_prives.id_dest
-			          WHERE (messages_prives.id_dest = $forum)
-			          GROUP BY id_mp
-			          ORDER BY messages_prives.date_mp";
 
-			$results=mysqli_query($link,$query);
-			echo '<table>';
-			while ($row=mysqli_fetch_array($results,MYSQLI_BOTH)){
-				$IDMP=$row['id_mp'];
-				$TEXTE=$row['texte'];
-				$DATE=$row['date'];
-				$NOM= $row['nom_ind'];
-				$PRENOM= $row['prenom'];
-				echo '<tr><th>'.$PRENOM.' '.$NOM.'<br/></th></tr>';
-				echo "<tr><td>$TEXTE<br/><i>$DATE</i> <br/></td><td></td></tr>";
-			}
-			echo '</table>';
+					//Requête pour récupérer tous les messages envoyés au forum.
 
-			echo '<form method="GET">';
+					$query="SELECT individus.id_ind, individus.nom_ind, individus.prenom,
+					          messages_prives.id_mp, DATE_FORMAT(messages_prives.date_mp, '%d/%m/%y %H:%i') as date, messages_prives.lu, messages_prives.texte, id_dest
+					          FROM individus
+					          JOIN messages_prives
+					          ON individus.id_ind = messages_prives.id_expe OR individus.id_ind = messages_prives.id_dest
+					          WHERE (messages_prives.id_dest = $forum)
+					          GROUP BY id_mp
+					          ORDER BY messages_prives.date_mp";
 
-			//En cliquant, le message 'Tapez votre message ici' par défault disparait. On entre le msg
-			?>
-			<input type="text" onfocus="this.value=''" value="Tapez votre message ici" size="80" name="message">
-			<?php
-			//On transmet en caché l'id du destinataire et de l'utilisateur connecté
-			echo "<input type='hidden' name='id_ind_co' value='$id_ind_co'>";
-			echo "<input type='hidden' name='idcontact' value='$forum'>";
-			echo '<input type="submit" value="Envoyer" name="bt">';
-			echo '</form>';
+					$results=mysqli_query($link,$query);
+					echo '<table>';
+					while ($row=mysqli_fetch_array($results,MYSQLI_BOTH)){
+						$IDMP=$row['id_mp'];
+						$TEXTE=$row['texte'];
+						$DATE=$row['date'];
+						$NOM= $row['nom_ind'];
+						$PRENOM= $row['prenom'];
+						echo '<tr><th>'.$PRENOM.' '.$NOM.'<br/></th></tr>';
+						echo "<tr><td>$TEXTE<br/><i>$DATE</i> <br/></td><td></td></tr>";
+					}
+					echo '</table>';
 
-			?>
+					echo '<form method="GET">';
+
+					//En cliquant, le message 'Tapez votre message ici' par défault disparait. On entre le msg
+					?>
+					<input type="text" onfocus="this.value=''" value="Tapez votre message ici" size="80" name="message">
+					<?php
+					//On transmet en caché l'id du destinataire et de l'utilisateur connecté
+					echo "<input type='hidden' name='id_ind_co' value='$id_ind_co'>";
+					echo "<input type='hidden' name='idcontact' value='$forum'>";
+					echo '<input type="submit" value="Envoyer" name="bt">';
+					echo '</form>';
+
+					?>
+			</div>
     </div>
   </div>
 
