266d89e3efc6f6059cb53590940e486f385a806a
Aurélie <aurelie.jambon@agro-bordeaux.fr>
2018-05-08 10:28:24 +0200

    qui sommes nous

---
+++ Racine/gabarits/description_NC.php
@@ -0,0 +1,23 @@
+<!-- Code effectué par Aurélie Jambon -->
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title> Présentation </title>
+		
+		<!-- css -->
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<link rel="stylesheet" href="css/style.css">
+	</head>
+	<body>
+	<?php include("Entete-NC.php"); ?>
+		</br>
+		</br> 
+		</br>
+		<div class="btn-group-vertical">
+			<button type="button" href="Presentation_NC.php" class="btn btn-secondary btn_lg">Accueil qui sommes-nous</button>
+			<button type="button" href="description_NC.php" class="btn btn-primary">Présentation</button>
+			<button type="button" href="mission_NC.php" class="btn btn-secondary">Notre mission</button>
+			<button type="button" href="contact_NC.php" class="btn btn-secondary">Nous contacter</button>
+		</div>
+	</body>
+</html>
