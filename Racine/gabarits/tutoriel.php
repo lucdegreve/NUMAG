9691dfbb7695f922e75b8885dc5066fa304f6ae3
Elsa <elsa.guilley@agro-bordeaux.fr>
2018-05-08 11:56:20 +0200

    Présentation tutoriel

--- Racine/gabarits/tutoriel.php
+++ Racine/gabarits/tutoriel.php
@@ -19,31 +19,44 @@
 <h1> Tutoriel </h1>
 <br/><br/>
 	
+	
+<div class="container-fluid">
 	<!-- Section à placer à gauche -->
-	<div id="guide">
-		<h3> Guide d'utilisation de RACINE </h3>
-		<br/><br/>
-		Vous trouverez ci-dessous un guide d'utilisation de la plateforme:
-		<br/><br/>
-		<iframe src="video/guide.pdf" width="800" height="800"></iframe>
-		<br/><br/>
+	<div class="col-lg-6">
+		<div id="guide">
+			<div class="jumbotron HauteurMax">
+				<h3> Guide d'utilisation de RACINE </h3>
+				<hr class="my-4">
+				<br/>
+				Vous trouverez ci-dessous un guide d'utilisation de la plateforme:
+				<br/><br/>
+				<iframe src="video/guide.pdf" width="800" height="800"></iframe>
+				<br/><br/>
+			</div>
+		</div>
+	</div>
+	<!-- Section à placer à droite, vidéos les unes en dessous des autres -->
+	<div class="col-lg-6">
+		<div id="video">
+			<div class="jumbotron HauteurMax">
+				<h3> Vidéos </h3>
+				<hr class="my-4">
+				<br/>
+				Et quelques vidéos pour une prise en main plus facile ! Vous pouvez les visualiser directement sur votre navigateur, ou les télécharger sur votre ordinateur.
+				<br/><br/>
+				<h5> 1ère vidéo </h5>
+				<br/>
+				<a href="video/Stageetconnexionfini.htm" target="_blank" > Lien vers la 1ère vidéo </a>
+				<br/>
+				<a href="video/Stageetconnexionfini.exe" > Téléchargement de la 1ère vidéo </a>
+				<br/><br/>
+				<h5> 2ème vidéo </h5>
+				<br/>
+			</div>
+		</div>
 	</div>
 	
-	<!-- Section à placer à droite, vidéos les unes en dessous des autres -->
-	<div id="video">
-		<h3> Vidéos </h3>
-		<br/><br/>
-		Et quelques vidéos pour une prise en main plus facile ! Vous pouvez les visualiser directement sur votre navigateur, ou les télécharger sur votre ordinateur.
-		<br/><br/>
-		<h5> 1ère vidéo </h5>
-		<br/>
-		<a href="video/Stageetconnexionfini.htm" target="_blank" > Lien vers la 1ère vidéo </a>
-		<br/>
-		<a href="video/Stageetconnexionfini.exe" > Téléchargement de la 1ère vidéo </a>
-		<br/><br/>
-		<h5> 2ème vidéo </h5>
-		<br/>
-	</div>
+</div>
 	
 	<h3> Bonne navigation sur RACINE ! </h3>
 	
