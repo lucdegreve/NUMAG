6e41a8f805f724b78a61bd9e125f0034831f6974
Aurélie <aurelie.jambon@agro-bordeaux.fr>
2018-05-08 10:38:26 +0200

    qui sommes nous

--- Racine/gabarits/presentation.php
+++ Racine/gabarits/presentation.php
@@ -17,9 +17,12 @@
 		<div class="btn-group-vertical">
 			<!-- <button type="button" href="Presentation.php" class="btn btn-primary btn_lg">Accueil qui sommes-nous</button> -->
 			<a href="Presentation.php" class="btn btn-primary btn-lg btn-block">Accueil qui sommes-nous</a>
-			<button type="button" href="description.php" class="btn btn-secondary">Présentation</button>
-			<button type="button" href="mission.php" class="btn btn-secondary">Notre mission</button>
-			<button type="button" href="contact.php" class="btn btn-secondary">Nous contacter</button>
+			<!-- <button type="button" href="description.php" class="btn btn-secondary">Présentation</button> -->
+			<a href="description.php" class="btn btn-secondary btn-lg btn-block">Présentation</a>
+			<!-- <button type="button" href="mission.php" class="btn btn-secondary">Notre mission</button> -->
+			<a href="description.php" class="btn btn-secondary btn-lg btn-block">Notre mission</a>
+			<!-- <button type="button" href="contact.php" class="btn btn-secondary">Nous contacter</button> -->
+			<a href="description.php" class="btn btn-secondary btn-lg btn-block">Nous contacter</a>
 		</div>
 	</body>
 </html>
